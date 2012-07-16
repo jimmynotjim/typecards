c4e7d8c5f68b8df21732469f54001b84d41b8226
James Wilson <jimmynotjim@yahoo.com>
2012-07-16 17:10:21 -0400

    Changed Flash Cards to All Cards and added My Cards for faved/saved cards

--- tc-beta-theme/includes/inc-site-nav.php
+++ tc-beta-theme/includes/inc-site-nav.php
@@ -1,7 +1,8 @@
 <nav class="site-nav">
 	<ul>
 		<li><input type="search" class="search-box" placeholder="Search"></li>
-		<li><a href="/anatomy" class="anatomy">Flash Cards</a></li>
+		<li><a href="/anatomy" class="anatomy">All Cards</a></li>
+		<li><a href="/mycards" class="my-cards">My Cards</a></li>
 		<li><a href="/quiz" class="quiz">Quiz</a></li>
 	</ul>
 </nav>
