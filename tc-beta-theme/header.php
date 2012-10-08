050b459235e7d35737bf93b4b6dbd1a7040c7a5e
James Wilson <jimmynotjim@yahoo.com>
2012-10-08 11:37:00 -0400

    Condensed viewport meta, hid splash screens, set redirect/overlay to home only

--- tc-beta-theme/header.php
+++ tc-beta-theme/header.php
@@ -3,7 +3,7 @@
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!-->
-<html class="no-js" lang="en" manifest="">
+<html class="no-js" lang="en">
 <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
@@ -12,33 +12,31 @@
 	<meta name="description" content="Master the finer details of typography">
 	<meta name="keywords" content="type, typography, study, cards">
 
-	<meta name="viewport" content="width=device-width">
-	<meta name="viewport" content="initial-scale=1">
-	<meta name="viewport" content="user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-	<link href="<?php bloginfo( 'template_url' ); ?>/assets/img/iphone-splash.png" media="(device-width: 320px)" rel="apple-touch-startup-image">
+<!--	<link href="<?php bloginfo( 'template_url' ); ?>/assets/img/iphone-splash.png" media="(device-width: 320px)" rel="apple-touch-startup-image">
 	<link href="<?php bloginfo( 'template_url' ); ?>/assets/img/iphone-splash.png" media="(device-width: 320px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
-	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php bloginfo( 'template_url' ); ?>/assets/img/icons/typecards-icon-114.png" />
+-->	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php bloginfo( 'template_url' ); ?>/assets/img/icons/typecards-icon-114.png" />
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php bloginfo( 'template_url' ); ?>/assets/img/icons/typecards-icon-144.png" />
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo( 'template_url' ); ?>/assets/img/icons/typecards-icon-114.png" />
 	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo( 'template_url' ); ?>/assets/img/icons/typecards-icon-144.png" />
 
-
 	<link rel="shortcut icon" type="image/ico" href="<?php bloginfo( 'template_url' ); ?>/assets/img/icons/favicon.ico" />
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-	<link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/assets/css/style.css?v=1" />
+	<link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/assets/css/style.css?v=1.2" />
 
 	<!-- Scripts -->
 
 	<!-- Modernizr -->
-	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/libs/modernizr-2.5.2.min.js?v=1"></script>
+	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/libs/modernizr-2.5.2.min.js"></script>
 
 	<!-- Typkit Code -->
 	<script type="text/javascript" src="http://use.typekit.com/fhw4ytw.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
 	<!-- iOS Detection -->
+	<?php if( is_home() ) { ?>
 	<script>
 		if (window.navigator.userAgent.indexOf('iPhone') != -1) {
 			if (window.navigator.standalone == true) {
@@ -50,6 +48,7 @@
 			document.location.href='http://typecardsapp.com';
 		}
 	</script>
+	<?php } ?>
 
 	<!-- Google Analytics -->
 	<script type="text/javascript">
