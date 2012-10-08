8f737f1168e405ec5c5b7d62c4bdffc00c07e0db
James Wilson <jimmynotjim@yahoo.com>
2012-10-08 11:39:09 -0400

    Set timeout on callback to prevent stuttering while sliding cards

--- tc-beta-theme/footer.php
+++ tc-beta-theme/footer.php
@@ -1,34 +1,33 @@
 	<!-- jquery -->
-<!--	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>	-->
-	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/libs/jquery-1.7.2.min.js?v=1"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
 
 	<!-- swipe.js -->
 	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/swipe.js?v=1"></script>
 	<script>
 		window.mySwipe = new Swipe(document.getElementById('slider'), {
 			gutter: 10,
-			callback: function(e, pos) {
-
-				var i = cards.length;
-				while (i--) {
-					cards[i].className = 'card-holder';
-				}
-				var c = e;
-				var p = c-1;
-				var n = c+1;
-				cards[c].className = 'card-holder on';
-				if( cards[p] != null) cards[p].className = 'card-holder on';
-				if( cards[n] != null) cards[n].className = 'card-holder on';
+			callback: function(e, pos) { //swap transform and backface styles on current, previous and next cards to prevent crashing
+				setTimeout(function(){ //timeout needed to prevent stuttering while swiping cards
+					var i = cards.length;
+					while (i--) {
+						cards[i].className = 'card-holder';
+					}
+					var c = e;
+					var p = c-1;
+					var n = c+1;
+					cards[c].className = 'card-holder on';
+					if( cards[p] != null) cards[p].className = 'card-holder on';
+					if( cards[n] != null) cards[n].className = 'card-holder on';
+				}, 1);
 			}
 		}),
 		cards = document.getElementById('slider').getElementsByTagName('li');
 	</script>
 
-	<!-- jgestures.js -->
-<!--	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/libs/jgestures.min.js"></script>	-->
-
 	<!-- custom scripts -->
 	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/scripts.js?v=1"></script>
+
 
   <?php wp_footer(); ?>
 
