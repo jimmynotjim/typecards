b56c868b01dbbe98d6e8f9656c42478e9e324dc3
James Wilson <jimmynotjim@yahoo.com>
2012-10-16 00:52:13 -0400

    Added password to all pages

--- tc-beta-theme/page.php
+++ tc-beta-theme/page.php
@@ -1,11 +1,23 @@
 <?php get_header(); ?>
 <div role="main">
+	<?php if ( is_user_logged_in() ) {
+		include_once( 'includes/inc-anatomy-cards.php' );
+	} else {
+		echo '<div class="card-holder"><section class="card"><article>';
+		echo '<h1 class="beta-logo">typecards &beta;</h1>';
+		wp_login_form();
+		echo '</article></section></div>';
+	} ?>
 	<?php
 		if ( is_page( 'anatomy' ) ) {
-			include_once( 'includes/inc-anatomy-cards.php' );
-		}
-		elseif ( is_page( 'quiz' ) ) {
-			include_once( 'includes/inc-quiz-cards.php' );
+			if ( is_user_logged_in() ) {
+				include_once( 'includes/inc-anatomy-cards.php' );
+			} else {
+				echo '<div class="card-holder"><section class="card"><article>';
+				echo '<h1 class="beta-logo">typecards &beta;</h1>';
+				wp_login_form();
+				echo '</article></section></div>';
+			}
 		}
 		else {
 			echo '<p>Nothing here</p>';
