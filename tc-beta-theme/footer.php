be1d5d219a1430a9dd48eed349a79b47318e8668
James Wilson <jimmynotjim@yahoo.com>
2013-03-17 23:04:50 -0400

    Final adjustments for beta push

--- tc-beta-theme/footer.php
+++ tc-beta-theme/footer.php
@@ -1,8 +1,8 @@
 	<!-- jquery cookie -->
 	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/jquery.cookie.js"></script>
 
-	<script src="http://rawgithub.com/twitter/typeahead.js/master/dist/typeahead.js"></script>
-	<script src="http://twitter.github.com/hogan.js/builds/2.0.0/hogan-2.0.0.js"></script>
+	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/libs/typeahead.js"></script>
+	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/libs/hogan-2.0.0.min.js"></script>
 
 	<script src="<?php bloginfo('template_url'); ?>/assets/js/FastClick.js"></script>
 	<script>
@@ -43,10 +43,10 @@
 		}),
 		cards = $('#anatomy_slider').find('.card-holder');
 
-		window.appSwipe = new Swipe(document.getElementById('app_slider'), {
+/*		window.appSwipe = new Swipe(document.getElementById('app_slider'), {
 			gutter: 10
 		});
-	</script>
+*/	</script>
 
 	<!-- custom scripts -->
 	<script src="<?php bloginfo( 'template_url' ); ?>/assets/js/scripts-ck.js"></script>
@@ -54,6 +54,7 @@
 	<script>
 		$('.search-terms').typeahead({
 			name: 'anatomy-terms',
+			limit: 4,
 			template: [
 				'<p onclick="anatomySwipe.slide({{id}}, 400);">{{value}}</p>'
 			],
--- tc-beta-theme/index.php
+++ tc-beta-theme/index.php
@@ -8,56 +8,29 @@
 						<h1 class="beta-logo">typecards</h1>
 						<nav class="app-nav">
 							<input class="search-terms" type="text" placeholder="Search Terms" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
-							<button id="all-cards">All Cards</button>
+							<button id="all-cards">All Cards</button><!--
 							<button onclick="appSwipe.slide(1, 400); return false;">Settings</button>
 							<button onclick="appSwipe.slide(1, 400); return false;">Instructions</button>
-							<button onclick="appSwipe.slide(1, 400); return false;">About</button>
+							<button onclick="appSwipe.slide(1, 400); return false;">About</button>-->
+							<button>...</button>
 						</nav>
-				<!--		<div class="text-size-controls">
-							<button id="size-smaller">Smaller</button>
-							<button id="size-larger">Larger</button>
-						</div>
-							<div>
-								<p>typecards began as a tool to help me better understand typography. With a little help from some good friends it's grown into a tool I hope you find useful too. Over time it will grow to include more than just Anatomy Terms and hopefully become your goto source on all things type.</p>
-								<p>This is a beta release, there's sure to be bugs and incomplete information. Please report any inconcistencies to <a href="mailto:help@typecardsapp.com">help@typecardsapp.com</a>.</p>
-							</div>
-						<aside class="terms-nav hidden">
-							<nav class="scrolling-list">
-								<ul class="menu">
-								<?php
-									$i = 0;
-									$anatomyArgs = array(
-										'post_type'			=> 'anatomy',
-										'posts_per_page'	=> -1,
-										'orderby'			=> 'title',
-										'order'				=> 'ASC'
-									);
-									query_posts($anatomyArgs);
-									if( have_posts() ) while( have_posts() ) : the_post();
-								?>
-									<li><a href="#" id="<?php echo $i; ?>" class="shortcut" onclick="anatomySwipe.slide(<?php echo $i; ?>, 400); return false;"><?php the_title(); ?></a></li>
-									<?php $i++; ?>
-								<?php
-									endwhile;
-									wp_reset_query();
-								?>
-								</ul>
-							</nav>
-							<button class="dismiss">Dismiss</button>
-						</aside>
-						<?php //if( $_COOKIE['dismissInstructions'] != 1 ) { ?>
+						<?php if( $_COOKIE['dismissInstructions'] != 1 ) { ?>
 						<aside class="instructions">
-							<span class="details">Swipe to navigate through the cards, tap to flip a card and view it's details.</span>
+							<span class="details">Swipe to navigate through the cards, tap to flip a card and view its details.</span>
 							<button class="dismiss">Dismiss</button>
 						</aside>
-						<?php //} ?>-->
+						<?php } ?>
 					</article>
 				</section>
 			</li>
-			<li class="card-holder">
+<!--			<li class="card-holder">
 				<section class="card">
 					<article>
 						<h1>About typecards</h1>
+						<div class="text-size-controls">
+							<button id="size-smaller">Smaller</button>
+							<button id="size-larger">Larger</button>
+						</div>
 						<p>typecards is an experiment using html and javascript to create a navtive app like experience, on any device. This is a beta release, there's sure to be bugs and incomplete information. Please report any inconcistencies to <a href="mailto:help@typecardsapp.com">help@typecardsapp.com</a>.</p>
 						<dl>
 							<dt>Web</dt><dd><a href="http://typecardsapp.com">typecardsapp.com</a></dd>
@@ -74,7 +47,7 @@
 
 					</article>
 				</section>
-			</li>
+			</li>-->
 		</ul>
 	</div>
 	<?php include_once( 'includes/inc-anatomy-cards.php' ); ?>
