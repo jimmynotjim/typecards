171cfff9cc89cd734e504f351ca2eee6ef115974
James Wilson <jimmynotjim@yahoo.com>
2013-08-15 19:33:41 -0400

    Swapped conditional for api

--- tc-beta-theme/page.php
+++ tc-beta-theme/page.php
@@ -1,14 +1,17 @@
 <?php
-if ( is_page( 'app' ) ) {
-get_header(); ?>
+	if ( is_page('api') ) :
+		header('Content-Type: application/json; charset=utf8');
+		json_function();
+	else :
+	get_header();
+?>
 
 <div role="main" class="main-app">
 	<?php include_once( 'includes/inc-app-cards.php'); ?>
 	<?php include_once( 'includes/inc-anatomy-cards.php' ); ?>
 </div>
 
-<?php get_footer();
-} elseif ( is_page('api') ) {
-    header('Content-Type: application/json; charset=utf8');
-	json_function();
-} ?>
+<?php
+	get_footer();
+	endif;
+?>
